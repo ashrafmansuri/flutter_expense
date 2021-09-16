570cb49b829593aad1aea68bd9938a182ec96697
Ashraf <iashrafmansuri@gmail.com>
2021-09-16 20:22:10 +0530

    New push

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import './widgets/userTranaction.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        //mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('CHART!'),
+              elevation: 5,
+            ),
+          ),
+          UserTransaction()
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/userTranaction.dart
@@ -0,0 +1,45 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import './transactionCard.dart';
+import './transactionList.dart';
+import '../models/transaction.dart';
+
+class UserTransaction extends StatefulWidget {
+  @override
+  _UserTransactionState createState() => _UserTransactionState();
+}
+
+class _UserTransactionState extends State<UserTransaction> {
+  final List<Transaction> _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void addTxToList(String item,double price)
+  {
+    var tx = Transaction(id: DateTime.now().toString(), title: item, amount: price, date: DateTime.now());
+    _transactions.add(tx);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TransactionCard(addTxToList),
+        TransactionList(
+          transactions: _transactions,
+        )
+      ],
+    );
+  }
+}
