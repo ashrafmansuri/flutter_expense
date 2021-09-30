69ab1cd05f4d9ba327bba11507e8a331144f6539
ashrafmansuri <ashrafmansuri@outlook.com>
2021-10-01 00:28:26 +0530

    added modal

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import './widgets/userTranaction.dart';
+import './widgets/transactionList.dart';
+import './models/transaction.dart';
+import './widgets/transactionCard.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,34 +9,91 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.yellow
+      ),
       title: 'Expense Calculator',
       home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void addTxToList(String item, double price) {
+    var tx = Transaction(
+        id: DateTime.now().toString(),
+        title: item,
+        amount: price,
+        date: DateTime.now());
+    setState(() {
+      _transactions.add(tx);
+    });
+  }
+
+ 
+  void _openModal(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (bCtx) {
+          return  TransactionCard(addTxToList);
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
-      ),
-      body: Column(
-        //mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-            width: double.infinity,
-            child: Card(
-              color: Colors.blue,
-              child: Text('CHART!'),
-              elevation: 5,
-            ),
-          ),
-          UserTransaction()
+        actions: [
+          IconButton(
+              onPressed: () => _openModal(context), icon: Icon(Icons.add))
         ],
+      ),
+      body: Container(
+        height: 500,
+        child: SingleChildScrollView(
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                child: Card(
+                  color: Colors.blue,
+                  child: Text('CHART!'),
+                  elevation: 5,
+                ),
+              ),
+              TransactionList(transactions: _transactions)
+            ],
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _openModal(context),
       ),
     );
   }
--- lib/widgets/transactionCard.dart
+++ lib/widgets/transactionCard.dart
@@ -1,13 +1,32 @@
-
 import 'package:flutter/material.dart';
 
-class TransactionCard extends StatelessWidget {
-  final _titleTextHandler = TextEditingController();
-  final _amountTextHandler = TextEditingController(); 
+class TransactionCard extends StatefulWidget {
   final Function _addTx;
 
   TransactionCard(this._addTx);
- 
+
+  @override
+  _TransactionCardState createState() => _TransactionCardState();
+}
+
+class _TransactionCardState extends State<TransactionCard> {
+  final _titleTextHandler = TextEditingController();
+
+  final _amountTextHandler = TextEditingController();
+
+  void _sumbitData()
+  {
+
+    if(_titleTextHandler.text.isEmpty || double.parse(_amountTextHandler.text) <=0)
+    {
+      return;
+    }
+         widget._addTx(_titleTextHandler.text,
+                      double.parse(_amountTextHandler.text));
+
+                      Navigator.of(context).pop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -19,13 +38,18 @@ class TransactionCard extends StatelessWidget {
             TextField(
               controller: _titleTextHandler,
               decoration: InputDecoration(labelText: "Title"),
+               onSubmitted: (_) {  _sumbitData();
+                }
             ),
             TextField(
-              controller: _amountTextHandler,
-              decoration: InputDecoration(labelText: "Price"),
-            ),
+                controller: _amountTextHandler,
+                decoration: InputDecoration(labelText: "Price"),
+                keyboardType: TextInputType.number,
+                onSubmitted: (val) {  _sumbitData();
+                }
+                ),
             TextButton(
-                onPressed:() => _addTx(_titleTextHandler.text,  double.parse(_amountTextHandler.text)) ,
+                onPressed: _sumbitData,
                 child: Text("Enter New"),
                 style: ButtonStyle(
                     foregroundColor: MaterialStateProperty.all(Colors.yellow))),
